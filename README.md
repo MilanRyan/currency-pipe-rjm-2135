182025fe6a3cd4e1dd58a5aefcebd1b4e18fd3af
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2023-02-26 19:51:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# currency-pipe-rjm-2135
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cv-2135-rjm-quhf5e)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = "Ryan Milan WD-301";
+}
+
