f7ce281e9a857bd8a13902b05566fa630e703dd1
MilanRyan <100083961+MilanRyan@users.noreply.github.com>
2023-02-26 21:21:13 +0800

    1 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component, NgModule, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = "Ryan Milan WD-301";
-}
+export class AppComponent {
+  title = 'pipes-reactive';
+  presentDate = new Date();
 
+  price: number = 2000;
+
+  Fruits = ['Apple', 'Orange', 'Grapes', 'Mango', 'Kiwi', 'pomegrante'];
+
+  todayO$ = new Observable<Date>((Observer) => {
+    setInterval(() => {
+      Observer.next(new Date());
+    }, 1000);
+  });
+
+  decimalNum1: number = 8.7598623;
+  decimalNum2: number = 5.43;
+  decimalNum3: number = 6.21;
+  decimalNum4: number = 7.34;
+  decimalNum5: number = 8.43;
+}
